da96ba03d21efad46d3413c17ae7a38b6cbe78c8
anntnzrb <anntnzrb@protonmail.com>
2021-06-22 08:12:29 -0500

    Prefer using './' for files in the PWD

--- build-iso.sh
+++ build-iso.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Write out the current channels to be included with the image
-guix describe -f channels > channels.scm
+guix describe -f channels > ./channels.scm
 
 # Build the image and copy it to the current directory
-image=$(guix system image -t iso9660 installer.scm)
+image=$(guix system image -t iso9660 ./installer.scm)
 echo "Built image: $image"
 cp $image ./guix-installer.iso
