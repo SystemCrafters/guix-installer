fb470444a1d759de1072a8f785cf90e7f7eacce1
Soham S Gumaste <sgumas2@uic.edu>
2024-05-10 02:39:18 -0500

    Add channels and substitutes in installer guix package

--- guix/installer.scm
+++ guix/installer.scm
@@ -1,27 +1,13 @@
-;;; Copyright © 2019 Alex Griffin <a@ajgrf.com>
-;;; Copyright © 2019 Pierre Neidhardt <mail@ambrevar.xyz>
-;;; Copyright © 2019 David Wilson <david@daviwil.com>
-;;;
-;;; This program is free software: you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation, either version 3 of the License, or
-;;; (at your option) any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 ;; Generate a bootable image (e.g. for USB sticks, etc.) with:
 ;; $ guix system image -t iso9660 installer.scm
 
 (define-module (nongnu system install)
+  #:use-module (gnu image)
   #:use-module (gnu services)
+  #:use-module (gnu services base)
   #:use-module (gnu system)
   #:use-module (gnu system install)
+  #:use-module (gnu system image)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages vim)
   #:use-module (gnu packages curl)
@@ -31,7 +17,31 @@
   #:use-module (gnu packages package-management)
   #:use-module (nongnu packages linux)
   #:use-module (guix)
+  #:use-module (guix channels)
   #:export (installation-os-nonfree))
+
+(define my-channels
+  (eval (read (open-input-file "./channels.scm")) (current-module)))
+
+(define my-nonguix
+  (guix-for-channels my-channels))
+
+(current-guix-package my-nonguix)
+
+(define %my-services
+  (modify-services (operating-system-user-services installation-os)
+    (guix-service-type config =>
+                       (guix-configuration
+                        (inherit config)
+                        (guix my-nonguix)
+                        (substitute-urls
+                         (append
+                          (list "https://substitutes.nonguix.org")
+                          %default-substitute-urls))
+                        (authorized-keys
+                         (append (list
+                                  (local-file "./signing-key.pub"))
+                                 %default-authorized-guix-keys))))))
 
 (define installation-os-nonfree
   (operating-system
@@ -49,11 +59,20 @@
       ;; Include the channel file so that it can be used during installation
       (simple-service 'channel-file etc-service-type
                       (list `("channels.scm" ,(local-file "channels.scm"))))
-      (operating-system-user-services installation-os)))
+      %my-services))
 
     ;; Add some extra packages useful for the installation process
     (packages
-     (append (list git curl stow vim emacs-no-x-toolkit)
+     (append (list git curl stow vim emacs-no-x-toolkit my-nonguix)
              (operating-system-packages installation-os)))))
 
 installation-os-nonfree
+;; (image
+;;        (inherit efi-disk-image)
+;;        (format 'iso9660)
+;;        (size (* 3 1024 1024))
+;;        (operating-system installation-os-nonfree))
+
+;; Local Variables:
+;; geiser-scheme-implementation: "guix repl"
+;; End:
